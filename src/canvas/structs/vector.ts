3bf75bf6e78ad035a3938c7c21a96eefea3bdaf4
Smoren <ofigate@gmail.com>
2022-03-19 22:20:33 +0300

    vector class added

---
+++ src/canvas/structs/vector.ts
@@ -0,0 +1,109 @@
+import { VectorArrayType, VectorInterface } from "../types";
+
+/**
+ * Vector class
+ * @public
+ */
+export default class Vector implements VectorInterface {
+  /**
+   * Coordinate X
+   */
+  public x: number;
+  /**
+   * Coordinate Y
+   */
+  public y: number;
+
+  /**
+   * Vector constructor
+   * @param x - coordinate X
+   * @param y - coordinate Y
+   */
+  constructor([x, y]: VectorArrayType) {
+    this.x = x;
+    this.y = y;
+  }
+
+  /**
+   * Add another vector to this vector
+   * @param v - vector to add
+   */
+  add(v: Vector): Vector {
+    this.x += v.x;
+    this.y += v.y;
+
+    return this;
+  }
+
+  /**
+   * Subtracts vector with another vector
+   * @param v - vector to subtract
+   */
+  sub(v: Vector): Vector {
+    this.x -= v.x;
+    this.y -= v.y;
+
+    return this;
+  }
+
+  /**
+   * Multiples vector by number
+   * @param mul - multiplier
+   */
+  mul(mul: number): Vector {
+    this.x *= mul;
+    this.y *= mul;
+
+    return this;
+  }
+
+  /**
+   * Divides vector by number
+   * @param div - divider
+   */
+  div(div: number): Vector {
+    this.x /= div;
+    this.y /= div;
+
+    return this;
+  }
+
+  /**
+   * Inverses vector
+   */
+  inverse(): Vector {
+    this.x = -this.x;
+    this.y = -this.y;
+
+    return this;
+  }
+
+  /**
+   * Returns the length of vector
+   */
+  len(): number {
+    return Math.sqrt(this.x*this.x + this.y*this.y);
+  }
+
+  /**
+   * Returns distance vector of this and another vector
+   * @param v - another vector
+   */
+  distance(v: Vector): Vector {
+    return this.clone().sub(v);
+  }
+
+  /**
+   * Clones vector
+   */
+  clone(): Vector {
+    return new Vector(this.toArray());
+  }
+
+  /**
+   * Converts vector to array
+   */
+  toArray(): VectorArrayType {
+    return [this.x, this.y];
+  }
+}
--- src/canvas/types.ts
+++ src/canvas/types.ts
@@ -5,6 +5,72 @@
 export type VectorArrayType = [number, number];
 
 /**
+ * Interface of vector
+ * @public
+ */
+export interface VectorInterface {
+  /**
+   * Coordinate X
+   */
+  x: number;
+  /**
+   * Coordinate Y
+   */
+  y: number;
+
+  /**
+   * Add another vector to this vector
+   * @param v - vector to add
+   */
+  add(v: VectorInterface): VectorInterface;
+
+  /**
+   * Subtracts vector with another vector
+   * @param v - vector to subtract
+   */
+  sub(v: VectorInterface): VectorInterface;
+
+  /**
+   * Multiples vector by number
+   * @param mul - multiplier
+   */
+  mul(mul: number): VectorInterface;
+
+  /**
+   * Divides vector by number
+   * @param div - divider
+   */
+  div(div: number): VectorInterface;
+
+  /**
+   * Inverses vector
+   */
+  inverse(): VectorInterface;
+
+  /**
+   * Returns the length of vector
+   */
+  len(): number;
+
+  /**
+   * Returns distance vector of this and another vector
+   * @param v - another vector
+   */
+  distance(v: VectorInterface): VectorInterface;
+
+  /**
+   * Clones vector
+   */
+  clone(): VectorInterface;
+
+  /**
+   * Converts vector to array
+   */
+  toArray(): VectorArrayType;
+}
+
+
+/**
  * On change handler for ViewObservable objects
  * @public
  * @param target - observable object
