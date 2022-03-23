51228b69237351ef5235062bb03a8393d286d26d
Ekatereina Shepeleva <shepeleva.ea@phystech.edu>
2022-03-23 15:22:30 +0300

    feat: add NeighborhoodBound

---
+++ src/canvas/structs/bounds/neighborhood-bound.ts
@@ -0,0 +1,29 @@
+import { VectorArrayType } from '../../types';
+import { BoundInterface, NeighborhoodBoundConfig } from '../../types/bound';
+
+export default class NeighborhoodBound implements BoundInterface {
+  /**
+   * Bound config
+   */
+  protected _config: NeighborhoodBoundConfig;
+
+  /**
+   * RectangularBound constructor
+   * @param config - bound config
+   */
+  constructor(config: NeighborhoodBoundConfig) {
+    this._config = config;
+  }
+
+  /**
+   * {@inheritDoc BoundInterface.includes}
+   */
+  includes(coords: VectorArrayType): boolean {
+    const x = coords[0];
+    const y = coords[1];
+    const centerX = this._config.position[0];
+    const centerY = this._config.position[1];
+
+    return (x - centerX) ** 2 + (y - centerY) ** 2 <= this._config.radius ** 2;
+  }
+}
